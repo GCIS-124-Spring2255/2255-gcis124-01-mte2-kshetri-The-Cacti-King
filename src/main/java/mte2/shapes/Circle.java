38cdafd358e285b4260cb2799e907961f7f29da5
Baden Joseph (RIT Student) <bcj9505@rit.edu>
2026-03-04 11:11:58 -0500

    Q1

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -4,18 +4,23 @@
 
 package mte2.shapes;
 
-public class Circle {
-// public class Circle implements Shape {
+public class Circle implements Shape {
+        private final double radius;
+        public Circle(double radius) {    this.radius = radius;    }
+
+        @Override
+        public double perimeter() {
+            return 2 * Math.PI * radius;
+        }
+        @Override
+        public double area() {
+            return Math.PI * (radius * radius);
+        }
+
     
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
-
-    // @Override
-    // @Override
-
     public static void main(String[] args) {
-        
-        // ...
-
+        Circle c = new Circle(25);
+        System.out.println(c.area());
+        System.out.println(c.perimeter());
     }
 }
--- src/main/java/mte2/shapes/Triangle.java
+++ src/main/java/mte2/shapes/Triangle.java
@@ -4,22 +4,25 @@
 
 package mte2.shapes;
 
-public class Triangle {
-// public class Triangle implements Shape {
-    // private double sideA;
-    // private double sideB;
-    // private double sideC;
+public class Triangle implements Shape {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
 
-    // public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
+    public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
 
-    // @Override
+    @Override
+    public double perimeter() {
+        return sideA + sideB + sideC;
+    }
 
-    // @Override
-    // public double area() {    double p = perimeter()  / 2;    }
+    @Override
+    public double area() {
+        double p = perimeter() / 2;
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
 
     public static void main(String[] args) {
-        
-        // ...
 
     }
 }
